86408c5b159998ad304346fb9180b22b69d4fe89
sophie950 <sueboo950950@gmail.com>
2024-10-03 00:20:14 -0500

    completed everyday object

--- week-1/everyday-objects-run.js
+++ week-1/everyday-objects-run.js
@@ -5,3 +5,9 @@ import { Car } from "everyday-objects.js";
 const myCar = new Car("Toyota", "Corolla", 2020);
 console.log(myCar.getCarInfo()); // Output: "2020 Toyota Corolla"
 myCar.startEngine(); // Output: "The engine of Toyota Corolla is now running."
+
+const myphone = newphone("Iphone","14",23);
+myphone.makecall(3200000)
+myphone.batterystatus()   
+
+const myBook = new Book
--- week-1/everyday-objects.js
+++ week-1/everyday-objects.js
@@ -26,18 +26,51 @@ class Car {
 // makeCall(number): Logs "Calling [number] from [brand] [model]."
 // batteryStatus(): Logs "Battery life is at [batteryLife]%."
 
+class Phone{
+  constructor(brand, model, batterylife){
+    this.brand = brand;
+    this.model = model;
+    this.batterylife = batterylife;
+  }
+  makecall(number){
+    console.log(`calling ${number} from ${this.brand} ${this.model}.`);
+  }
+  batterystatus(){
+    console.log(`Battery life is at ${this.batterylife}%`);
+  }
+}
 // Class: Book
 // Properties: title, author, pages
 // Methods:
 // getSummary(): Returns a string with the book’s title and author (e.g., "The Great Gatsby by F. Scott Fitzgerald").
 // readPage(): Logs "You read a page from [title]."
 
+class Book{
+  constructor(title, author,pages){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+  }
+  getsummary(){
+    return `${this.title} by ${this.author}.`;
+  }
+
+  readpage() {
+    console.log(`you read a page from ${this.title}.`);
+  }
+}
 // Class: Laptop
 // Properties: brand, model, ramSize
 // Methods:
 // bootUp(): Logs "The [brand] [model] is booting up with [ramSize]GB RAM."
 // shutDown(): Logs "The [brand] [model] is shutting down."
-
+class Laptop{
+  constructor(brand, model,ramsize){
+    this.brand = brand;
+    this.model = model;
+    this.ramsize = ramsize;
+}
+}
 // Class: Backpack
 // Properties: brand, color, capacity
 // Methods:
